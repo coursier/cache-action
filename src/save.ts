d4783657a245c2ee681960495cdb2196db76561c
Alexandre Archambault <alexandre.archambault@gmail.com>
2020-08-17 16:00:07 +0200

    Save ~/.sbt and ~/.mill too

--- src/save.ts
+++ src/save.ts
@@ -3,22 +3,29 @@
 import * as cache from '@actions/cache'
 import * as core from '@actions/core'
 
-async function run(): Promise<void> {
-  const cacheKey = core.getState('COURSIER_CACHE_RESULT')
-  const primaryKey = core.getState('COURSIER_CACHE_KEY')
+async function restoreCache(id: string): Promise<void> {
+  const upperId = id.toLocaleUpperCase('en-US')
+  const primaryKey = core.getState(`${upperId}_CACHE_KEY`)
+
+  if (!primaryKey) {
+    return
+  }
+
+  const cacheKey = core.getState(`${upperId}_CACHE_RESULT`)
   const cachePaths = JSON.parse(
-    core.getState('COURSIER_CACHE_PATHS')
+    core.getState(`${upperId}_CACHE_PATHS`)
   ) as string[]
 
   if (cacheKey === primaryKey) {
     core.info(
-      `Cache hit occurred on the primary key ${primaryKey}, not saving cache.`
+      `Cache hit occurred on the primary key ${primaryKey}, not saving ${id} cache.`
     )
     return
   }
 
   // https://github.com/actions/cache/blob/9ab95382c899bf0953a0c6c1374373fc40456ffe/src/save.ts#L39-L49
   try {
+    core.info(`Saving ${id} cache`)
     await cache.saveCache(cachePaths, primaryKey)
   } catch (error) {
     if (error.name === cache.ValidationError.name) {
@@ -29,6 +36,12 @@ async function run(): Promise<void> {
       core.info(`[warning] ${error.message}`)
     }
   }
+}
+
+async function run(): Promise<void> {
+  await restoreCache('coursier')
+  await restoreCache('sbt')
+  await restoreCache('mill')
 }
 
 async function doRun(): Promise<void> {
